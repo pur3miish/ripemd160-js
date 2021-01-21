de828f8ce75f4419dbd31c7f3b8aa16f95962aa3
H <pur3miish@me.com>
2021-01-21 12:30:24 +1000

    C Code is not housed in separate code repo.

--- src/readme.md
+++
@@ -1,3 +0,0 @@
-# Setup
-
-This section describes how to build the C src files into a WASM binary.
--- wasm.sh
+++
@@ -1,2 +0,0 @@
-clang --target=wasm32 -nostartfiles --no-standard-libraries -Wl,--export="digest" -Wl,--export="get_ptr" -Wl,--no-entry -o wasm/ripemd160.wasm src/main.c
-node ./parse_wasm.js
